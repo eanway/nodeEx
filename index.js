9d5742047de0a73101294addb0d5fb0274f336c9
Evan Anway <evananway@evan-anways-macbook-pro.local>
2019-07-27 14:24:19 -0400

    removed old reference to axios

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const hbs = require("hbs")
 const app = express()
-const axios = require("./axios")
 const fetch = require('node-fetch');
 
 const port = process.env.PORT || 3000;
